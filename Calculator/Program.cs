5f6d48cb4dc6b614e56f771357f8f0d551d7f9f6
Michael Kragh <mikr71394@edu.ucl.dk>
2025-04-07 00:18:56 +0200

    calculator finish

---
+++ Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Calculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter another number:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine(num1 + num2);
+        
+
+        Console.ReadLine();
+    }
+}
